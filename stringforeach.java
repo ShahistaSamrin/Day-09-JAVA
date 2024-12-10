05d2d03b7874b3709e7070d288f0165a4ffb8394
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-10 19:54:29 +0530

    Add files via upload

---
+++ stringforeach.java
@@ -0,0 +1,10 @@
+/*  Apple,orange,mango  */
+import java.util.*;
+public class stringforeach{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    String a[]={"Apple","orange","mango"};
+    for(String b:a)
+    System.out.print(b+",");
+  }
+}
