ad04d395c33b33d9bfde52b3c4405c378f35164b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-10 19:56:15 +0530

    Add files via upload

---
+++ upperorlowerstring.java
@@ -0,0 +1,11 @@
+/* Shahista  //input   shahi//input
+   UPPERCASE            LOWERCASE      */
+import java.util.*;
+public class upperorlowerstring{
+  public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+    char s=sc.next().charAt(0);
+    if((s&32)==0) System.out.println("UPPER CASE");
+    else System.out.println("LOWER CASE"); 
+   }
+}
