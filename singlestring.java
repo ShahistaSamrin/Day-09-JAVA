deb28371a7d5b0a5e07a04ff792d3092f1d5c603
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-10 19:53:48 +0530

    Add files via upload

---
+++ singlestring.java
@@ -0,0 +1,14 @@
+/* shahi is good girl //input
+   shahi  //output
+   is good girl  */
+import java.util.*;
+public class singlestring{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    //single string in a sentence
+    String a=s.next();  //prints only shahi
+    System.out.println(a);
+    String b=s.nextLine(); //prints hole line
+    System.out.println(b);
+  }
+}
